b0be640a30568891a9487e71356d95497a57af94
Max Rumsey <m.rumsey04@gmail.com>
2024-04-13 21:22:18 +1000

    prevent tabbing on mainform

--- OzStripsGUI/BayControl.Designer.cs
+++ OzStripsGUI/BayControl.Designer.cs
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.lb_bay_name = new System.Windows.Forms.Label();
-            this.bt_div = new System.Windows.Forms.Button();
             this.flp_stripbay = new maxrumsey.ozstrips.gui.CustomFLP();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.bt_queue = new System.Windows.Forms.Button();
+            this.bt_div = new System.Windows.Forms.Button();
+            this.lb_bay_name = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -49,6 +49,18 @@
             this.panel1.Size = new System.Drawing.Size(481, 322);
             this.panel1.TabIndex = 0;
             // 
+            // flp_stripbay
+            // 
+            this.flp_stripbay.AutoScroll = true;
+            this.flp_stripbay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flp_stripbay.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
+            this.flp_stripbay.Location = new System.Drawing.Point(0, 42);
+            this.flp_stripbay.Name = "flp_stripbay";
+            this.flp_stripbay.Size = new System.Drawing.Size(481, 280);
+            this.flp_stripbay.TabIndex = 1;
+            this.flp_stripbay.WrapContents = false;
+            this.flp_stripbay.Click += new System.EventHandler(this.lb_bay_name_Click);
+            // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
@@ -60,6 +72,30 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(481, 42);
             this.panel2.TabIndex = 0;
+            // 
+            // bt_queue
+            // 
+            this.bt_queue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bt_queue.Location = new System.Drawing.Point(264, 4);
+            this.bt_queue.Name = "bt_queue";
+            this.bt_queue.Size = new System.Drawing.Size(172, 32);
+            this.bt_queue.TabIndex = 2;
+            this.bt_queue.TabStop = false;
+            this.bt_queue.Text = "Queue";
+            this.bt_queue.UseVisualStyleBackColor = true;
+            this.bt_queue.Click += new System.EventHandler(this.bt_queue_Click);
+            // 
+            // bt_div
+            // 
+            this.bt_div.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bt_div.Location = new System.Drawing.Point(442, 4);
+            this.bt_div.Name = "bt_div";
+            this.bt_div.Size = new System.Drawing.Size(36, 32);
+            this.bt_div.TabIndex = 1;
+            this.bt_div.TabStop = false;
+            this.bt_div.Text = "Div";
+            this.bt_div.UseVisualStyleBackColor = true;
+            this.bt_div.Click += new System.EventHandler(this.button1_Click);
             // 
             // lb_bay_name
             // 
@@ -73,40 +109,6 @@
             this.lb_bay_name.Text = "Bay Name";
             this.lb_bay_name.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.lb_bay_name.Click += new System.EventHandler(this.lb_bay_name_Click);
-            // 
-            // bt_div
-            // 
-            this.bt_div.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.bt_div.Location = new System.Drawing.Point(442, 4);
-            this.bt_div.Name = "bt_div";
-            this.bt_div.Size = new System.Drawing.Size(36, 32);
-            this.bt_div.TabIndex = 1;
-            this.bt_div.Text = "Div";
-            this.bt_div.UseVisualStyleBackColor = true;
-            this.bt_div.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // flp_stripbay
-            // 
-            this.flp_stripbay.AutoScroll = true;
-            this.flp_stripbay.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flp_stripbay.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
-            this.flp_stripbay.Location = new System.Drawing.Point(0, 42);
-            this.flp_stripbay.Name = "flp_stripbay";
-            this.flp_stripbay.Size = new System.Drawing.Size(481, 280);
-            this.flp_stripbay.TabIndex = 1;
-            this.flp_stripbay.WrapContents = false;
-            this.flp_stripbay.Click += new System.EventHandler(this.lb_bay_name_Click);
-            // 
-            // bt_queue
-            // 
-            this.bt_queue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.bt_queue.Location = new System.Drawing.Point(264, 4);
-            this.bt_queue.Name = "bt_queue";
-            this.bt_queue.Size = new System.Drawing.Size(172, 32);
-            this.bt_queue.TabIndex = 2;
-            this.bt_queue.Text = "Queue";
-            this.bt_queue.UseVisualStyleBackColor = true;
-            this.bt_queue.Click += new System.EventHandler(this.bt_queue_Click);
             // 
             // BayControl
             // 
--- OzStripsGUI/MainForm.Designer.cs
+++ OzStripsGUI/MainForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.pl_controlbar = new System.Windows.Forms.Panel();
+            this.bt_inhibit = new System.Windows.Forms.Button();
             this.pl_ad = new System.Windows.Forms.Panel();
             this.lb_ad = new System.Windows.Forms.Label();
             this.pl_stat = new System.Windows.Forms.Panel();
@@ -40,7 +41,7 @@
             this.ts_ad = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
             this.flp_main = new System.Windows.Forms.FlowLayoutPanel();
-            this.bt_inhibit = new System.Windows.Forms.Button();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.pl_controlbar.SuspendLayout();
             this.pl_ad.SuspendLayout();
             this.pl_stat.SuspendLayout();
@@ -61,6 +62,18 @@
             this.pl_controlbar.Name = "pl_controlbar";
             this.pl_controlbar.Size = new System.Drawing.Size(1784, 45);
             this.pl_controlbar.TabIndex = 0;
+            // 
+            // bt_inhibit
+            // 
+            this.bt_inhibit.Font = new System.Drawing.Font("Terminus (TTF)", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_inhibit.Location = new System.Drawing.Point(347, 3);
+            this.bt_inhibit.Name = "bt_inhibit";
+            this.bt_inhibit.Size = new System.Drawing.Size(96, 37);
+            this.bt_inhibit.TabIndex = 3;
+            this.bt_inhibit.TabStop = false;
+            this.bt_inhibit.Text = "Inhibit";
+            this.bt_inhibit.UseVisualStyleBackColor = true;
+            this.bt_inhibit.Click += new System.EventHandler(this.bt_inhibit_Click);
             // 
             // pl_ad
             // 
@@ -149,7 +162,8 @@
             // ts_ad
             // 
             this.ts_ad.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripTextBox1});
+            this.toolStripTextBox1,
+            this.toolStripSeparator1});
             this.ts_ad.Name = "ts_ad";
             this.ts_ad.Size = new System.Drawing.Size(79, 20);
             this.ts_ad.Text = "Aerodrome";
@@ -174,16 +188,10 @@
             this.flp_main.TabIndex = 2;
             this.flp_main.WrapContents = false;
             // 
-            // bt_inhibit
+            // toolStripSeparator1
             // 
-            this.bt_inhibit.Font = new System.Drawing.Font("Terminus (TTF)", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bt_inhibit.Location = new System.Drawing.Point(347, 3);
-            this.bt_inhibit.Name = "bt_inhibit";
-            this.bt_inhibit.Size = new System.Drawing.Size(96, 37);
-            this.bt_inhibit.TabIndex = 3;
-            this.bt_inhibit.Text = "Inhibit";
-            this.bt_inhibit.UseVisualStyleBackColor = true;
-            this.bt_inhibit.Click += new System.EventHandler(this.bt_inhibit_Click);
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
             // 
             // MainForm
             // 
@@ -228,6 +236,7 @@
         private System.Windows.Forms.Label lb_ad;
         private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
         private System.Windows.Forms.Button bt_inhibit;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
     }
 }
 
