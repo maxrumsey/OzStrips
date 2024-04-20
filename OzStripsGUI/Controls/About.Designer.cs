72b928fd507fd0c7465689ac1b5517b4601b88e8
Max Rumsey <m.rumsey04@gmail.com>
2024-04-20 22:55:00 +1000

    add thank you message in about

--- OzStripsGUI/Controls/About.Designer.cs
+++ OzStripsGUI/Controls/About.Designer.cs
@@ -28,54 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lb_version = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.groupBox1.SuspendLayout();
-            this.SuspendLayout();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            lb_version = new System.Windows.Forms.Label();
+            richTextBox1 = new System.Windows.Forms.RichTextBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.lb_version);
-            this.groupBox1.Controls.Add(this.richTextBox1);
-            this.groupBox1.Location = new System.Drawing.Point(4, 4);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.groupBox1.Size = new System.Drawing.Size(269, 254);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "About";
+            groupBox1.Controls.Add(lb_version);
+            groupBox1.Controls.Add(richTextBox1);
+            groupBox1.Location = new System.Drawing.Point(4, 4);
+            groupBox1.Name = "groupBox1";
+            groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            groupBox1.Size = new System.Drawing.Size(269, 254);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "About";
             // 
             // lb_version
             // 
-            this.lb_version.AutoSize = true;
-            this.lb_version.Location = new System.Drawing.Point(7, 224);
-            this.lb_version.Name = "lb_version";
-            this.lb_version.Size = new System.Drawing.Size(42, 13);
-            this.lb_version.TabIndex = 1;
-            this.lb_version.Text = "Version";
+            lb_version.AutoSize = true;
+            lb_version.Location = new System.Drawing.Point(7, 224);
+            lb_version.Name = "lb_version";
+            lb_version.Size = new System.Drawing.Size(42, 13);
+            lb_version.TabIndex = 1;
+            lb_version.Text = "Version";
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(7, 20);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(256, 197);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "OzStrips\n\nA stripboard extension for VATSYS.\n\n© Max Rumsey";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            richTextBox1.Location = new System.Drawing.Point(7, 20);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new System.Drawing.Size(256, 197);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "OzStrips\n\nA stripboard extension for VATSYS.\n\nSpecial thanks to Levi, Plane Alex, Cat Alex and Glenn for their help and support!\n\n© Max Rumsey";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
             // 
             // About
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.groupBox1);
-            this.Name = "About";
-            this.Size = new System.Drawing.Size(276, 261);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            Controls.Add(groupBox1);
+            Name = "About";
+            Size = new System.Drawing.Size(276, 261);
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
