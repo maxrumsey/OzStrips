a3bb743091fe2a2e1a6f153ef29bc27ef5f39068
Max Rumsey <m.rumsey04@gmail.com>
2024-07-19 16:51:07 +1000

    prevent alerts triggering for arrs

--- OzStripsGUI/OzStripsConfig.Designer.cs
+++ OzStripsGUI/OzStripsConfig.Designer.cs
@@ -70,7 +70,7 @@ namespace MaxRumsey.OzStripsPlugin.Gui {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to http://dev1.maxrumsey.xyz:80/.
+        ///   Looks up a localized string similar to http://localhost:8069.
         /// </summary>
         public static string socketioaddr {
             get {
@@ -79,7 +79,7 @@ namespace MaxRumsey.OzStripsPlugin.Gui {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.3.7.
+        ///   Looks up a localized string similar to 0.4.0.
         /// </summary>
         public static string version {
             get {
