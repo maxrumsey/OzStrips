b73b16aebc51ffa5b888d8fc45ff6fa7d27e771e
Max Rumsey <m.rumsey04@gmail.com>
2024-10-12 20:16:52 +1100

    Bump version

--- OzStripsGUI/OzStripsConfig.Designer.cs
+++ OzStripsGUI/OzStripsConfig.Designer.cs
@@ -79,7 +79,7 @@ namespace MaxRumsey.OzStripsPlugin.Gui {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.4.5.
+        ///   Looks up a localized string similar to 0.4.6.
         /// </summary>
         public static string version {
             get {
