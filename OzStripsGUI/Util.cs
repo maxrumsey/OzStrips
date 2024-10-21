517f9e055e29cf9851140f589d85f758c220650b
Max Rumsey <m.rumsey04@gmail.com>
2024-10-21 15:02:38 +1100

    Fix annoying error

--- OzStripsGUI/Util.cs
+++ OzStripsGUI/Util.cs
@@ -4,7 +4,6 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Newtonsoft.Json;
-using SocketIOClient.Transport.Http;
 using vatsys;
 
 namespace MaxRumsey.OzStripsPlugin.Gui;
