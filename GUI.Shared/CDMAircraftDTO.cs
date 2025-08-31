64dcd141dd4a4d68739d16e4df7cee43f8625652
Max Rumsey <max@maxrumsey.xyz>
2025-09-01 06:58:48 +1000

    Initial CDM framework

---
+++ GUI.Shared/CDMAircraftDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxRumsey.OzStripsPlugin.GUI.Shared;
+
+/// <summary>
+/// An aircraft in the CDM system.
+/// </summary>
+public class CDMAircraftDTO
+{
+    public StripKey Key { get; set; } = new();
+
+    public string RWY { get; set; } = string.Empty;
+
+    public CDMSlotTypes SlotType { get; set; } = CDMSlotTypes.NORMAL;
+}
---
+++ GUI.Shared/CDMSlotTypes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxRumsey.OzStripsPlugin.GUI.Shared;
+
+/// <summary>
+/// Types of CDM slots, with associated priority values.
+/// </summary>
+public enum CDMSlotTypes
+{
+    /// <summary>
+    /// Max priority.
+    /// </summary>
+    PRIORITY = 100,
+
+    /// <summary>
+    /// Aircraft waiting for a long time.
+    /// </summary>
+    LONGWAIT = 80,
+
+    /// <summary>
+    /// Aircraft with booked slots.
+    /// </summary>
+    BOOKED = 50,
+
+    /// <summary>
+    /// Aircraft with prebooked slots, but not "in the system" yet.
+    /// </summary>
+    PREBOOKED = 50,
+
+    /// <summary>
+    /// Normal aircraft.
+    /// </summary>
+    NORMAL = 30,
+
+    /// <summary>
+    /// Early with a slot.
+    /// </summary>
+    EARLY_NONCOMPLIANT = 30,
+}
