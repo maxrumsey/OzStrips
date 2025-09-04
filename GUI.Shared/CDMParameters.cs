8abd02a594bcac3d4e117fd6fe4e6e781c7be2b8
maxrumsey <max@maxrumsey.xyz>
2025-09-04 10:59:20 +1000

    add cdm parameters, restructure some cdm processor stuff

---
+++ GUI.Shared/CDMParameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxRumsey.OzStripsPlugin.GUI.Shared;
+
+/// <summary>
+/// Contains parameters for CDM operations at a specific aerodrome.
+/// </summary>
+public class CDMParameters
+{
+    /// <summary>
+    /// Gets or sets the default DAR.
+    /// </summary>
+    public TimeSpan? DefaultRate { get; set; }
+}
