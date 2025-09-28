9c2d2c8f4bc1e79c2d362b46c28dff60e855954b
Max Rumsey <max@maxrumsey.xyz>
2025-09-28 23:24:13 +1000

    fix server url

--- GUI/OzStripsConfig.Designer.cs
+++ GUI/OzStripsConfig.Designer.cs
@@ -79,7 +79,7 @@ namespace MaxRumsey.OzStripsPlugin.GUI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to http://localhost:5153/.
+        ///   Looks up a localized string similar to https://ozstripsserver.maxrumsey.xyz/.
         /// </summary>
         public static string socketioaddr {
             get {
