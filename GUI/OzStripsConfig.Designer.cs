1c95e524f1bed143716572e173d1162dc612ac47
Max Rumsey <max@maxrumsey.xyz>
2025-10-21 13:02:22 +1100

    bump ver to v0.7,2

--- GUI/OzStripsConfig.Designer.cs
+++ GUI/OzStripsConfig.Designer.cs
@@ -88,7 +88,7 @@ namespace MaxRumsey.OzStripsPlugin.GUI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.7.1.
+        ///   Looks up a localized string similar to 0.7.2.
         /// </summary>
         public static string version {
             get {
