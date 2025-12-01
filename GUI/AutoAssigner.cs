9279a2079c16b6e766abc83f7a915b40e72cd07e
maxrumsey <max@maxrumsey.xyz>
2025-12-01 17:35:40 +1100

    fix NRE error caused by unknown types.

--- GUI/AutoAssigner.cs
+++ GUI/AutoAssigner.cs
@@ -132,9 +132,17 @@ internal class AutoAssigner
 
     internal bool CompliesWithCondition(RuleCondition rule, AssignmentResult result, Strip strip, bool matchAsTrue = true)
     {
-        if (!string.IsNullOrEmpty(rule.IsJet) && Performance.GetPerformanceData(strip.FDR.AircraftTypeAndWake)?.IsJet != matchAsTrue)
+        if (!string.IsNullOrEmpty(rule.IsJet))
         {
-            return false;
+            var type = strip.FDR.AircraftTypeAndWake;
+
+            // if we can't find the type, assume its a prop.
+            var isJet = type is not null && Performance.GetPerformanceData(type)!.IsJet;
+
+            if (isJet != matchAsTrue)
+            {
+                return false;
+            }
         }
 
         if (!string.IsNullOrEmpty(rule.VFR) && (strip.FDR.FlightRules == "V") != matchAsTrue)
