6a57708a53ff89c5379cc78105f046a42c06890d
maxrumsey <max@maxrumsey.xyz>
2025-12-07 15:21:48 +1100

    initial drop down menu work

---
+++ GUI/Controls/DropDown.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using vatsys;
+
+namespace MaxRumsey.OzStripsPlugin.GUI.Controls;
+public partial class DropDown : BaseForm
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DropDown"/> class.
+    /// </summary>
+    /// <param name="items">List of drop down items.</param>
+    /// <param name="title">Element title.</param>
+    public DropDown(DropDownItem[] items, string title)
+    {
+        InitializeComponent();
+        Setup();
+        Text = title;
+
+        foreach (var item in items)
+        {
+            AddElement(item);
+        }
+    }
+
+    /// <summary>
+    /// Called when the drop down is completed, and will return a value.
+    /// </summary>
+    public event EventHandler<string>? Complete;
+
+    private void Setup()
+    {
+        MiddleClickClose = true;
+        HasMinimizeButton = false;
+        HasMaximizeButton = false;
+    }
+
+    private void AddElement(DropDownItem item)
+    {
+        Control element;
+        switch (item.Type)
+        {
+            case DropDownItem.DropDownItemType.BUTTON:
+                element = new GenericButton();
+                element.Text = item.Text;
+                element.Size = new(100, 28);
+                break;
+            case DropDownItem.DropDownItemType.FREETEXT:
+                var tb = new TextBox();
+                element = tb;
+                tb.Text = item.Text;
+                tb.Size = new(100, 28);
+                break;
+            default:
+                throw new InvalidOperationException("Unknown drop down item type");
+        }
+
+        element.MouseUp += Element_MouseUp;
+
+        flowLayoutPanel1.Controls.Add(element);
+    }
+
+    private void Element_MouseUp(object sender, MouseEventArgs e)
+    {
+        var control = sender as Control ?? throw new Exception("Sender was null.");
+        if (e.Button == MouseButtons.Left)
+        {
+            Complete?.Invoke(this, control.Text);
+            Close();
+        }
+        else if (e.Button == MouseButtons.Middle)
+        {
+            Close();
+        }
+    }
+}
---
+++ GUI/Controls/DropDownItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using static MaxRumsey.OzStripsPlugin.GUI.Controls.DropDownItem;
+
+namespace MaxRumsey.OzStripsPlugin.GUI.Controls;
+
+/// <summary>
+/// Drop down item used by the DropDown Form.
+/// </summary>
+public class DropDownItem(DropDownItemType type, string text)
+{
+    /// <summary>
+    /// Gets or sets the type of drop down item.
+    /// </summary>
+    public DropDownItemType Type { get; set; } = type;
+
+    /// <summary>
+    /// Gets or sets the text of the drop down item.
+    /// </summary>
+    public string Text { get; set; } = text;
+
+    /// <summary>
+    /// Possible drop down item types.
+    /// </summary>
+    public enum DropDownItemType
+    {
+        /// <summary>
+        /// Button item.
+        /// </summary>
+        BUTTON,
+
+        /// <summary>
+        /// Freetext element.
+        /// </summary>
+        FREETEXT,
+    }
+}
