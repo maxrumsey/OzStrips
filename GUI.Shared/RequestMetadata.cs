ddae548613b64efa6a717fe14b0fb04c868881e3
Max Rumsey <max@maxrumsey.xyz>
2026-04-16 12:28:54 +1000

    add v2 hub

---
+++ GUI.Shared/RequestMetadata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxRumsey.OzStripsPlugin.GUI.Shared;
+
+/// <summary>
+/// Metadata included with every request.
+/// </summary>
+public class RequestMetadata
+{
+    /// <summary>
+    /// Gets or sets the aerodrome icao code.
+    /// </summary>
+    public string AerodromeICAO { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the server code.
+    /// </summary>
+    public int ServerCode { get; set; }
+}
